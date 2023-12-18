bdef3c3161ae80760e0d029e956a2852d66b5c1a
Clarice Santos <clariceks8@gmail.com>
2023-12-18 11:44:02 -0300

    novo estilo e responsividade p is cads

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,23 +1,24 @@
 import "./card.css";
 
 export function Card({ name, description, image, status }) {
-  const imageUrl = image ? `data:image/jpeg;base64,${image}` : '';
+  const imageUrl = image ? `data:image/jpeg;base64,${image}` : "";
   return (
-   
-
-    <div className="card">    
+    <div className="card">
       <img src={imageUrl} alt={`${name} task`} />
 
-      <h2>{name}</h2>
+      <h3>{name}</h3>
       <p>
         <b>Descrição: </b>
         {description}
       </p>
-      <p>
-        <b>Status: </b>
-        {status}
-      </p>
+      <span>
+        <b>Status: </b> {status}
+      </span>
+
+      <div class="buttons">
+        <button class="button-edit">Editar</button>
+        <button class="button-delete">Excluir</button>
+      </div>
     </div>
-    
   );
 }
